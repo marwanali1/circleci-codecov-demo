680667504833d3b3e8f3532acf3c0cc2fd71eeb5
Marwan Ali <marwanali2428@gmail.com>
2019-08-06 22:40:39 -0400

    Write and test MathFunctions class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # circleci_test
+
+CircleCI integration test project
---
+++ test_math.py
@@ -0,0 +1,21 @@
+import unittest
+from math import MathFunctions
+
+
+class TestMathFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(MathFunctions.add(3, 4), 7)
+
+    def test_sub(self):
+        self.assertEqual(MathFunctions.subtract(4, 3), 1)
+
+    def test_mult(self):
+        self.assertEqual(MathFunctions.multiply(3, 4), 12)
+
+    def test_div(self):
+        self.assertEqual(MathFunctions.divide(6, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
