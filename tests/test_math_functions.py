a55b09af945434e09977f54a351df2643a5488e0
Marwan Ali <marwanali2428@gmail.com>
2019-08-09 23:36:42 -0400

    Remove test

--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -13,8 +13,8 @@ class TestMathFunctions(unittest.TestCase):
     def test_mult(self):
         self.assertEqual(MathFunctions.multiply(3, 4), 12)
 
-    def test_div(self):
-        self.assertEqual(MathFunctions.divide(6, 2), 3)
+    # def test_div(self):
+    #     self.assertEqual(MathFunctions.divide(6, 2), 3)
 
 
 if __name__ == '__main__':
